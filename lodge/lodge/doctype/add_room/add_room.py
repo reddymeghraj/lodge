ff3772b93d1b4c4174612d364e02b236398d241b
reddy <reddymeghraj@gmail.com>
2015-11-19 15:05:00 +0530

    [App for lodge]

--- lodge/lodge/doctype/add_room/add_room.py
+++ lodge/lodge/doctype/add_room/add_room.py
@@ -11,3 +11,12 @@ class AddRoom(Document):
 		q=frappe.db.sql("""select room_no from `tabAdd Room` where room_no=%s""",(room))
 		if q:
 			frappe.throw("Entered Room No already exists!")
+
+@frappe.whitelist()
+def check_roomno(r):
+	q = frappe.db.sql("""select name from `tabAdd Room` where room_no=%s""",(r))
+	if q:
+		frappe.msgprint("Entered Room no already exists")
+		return 1
+	else:
+		return 2
