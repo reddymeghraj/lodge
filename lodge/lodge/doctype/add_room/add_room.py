f90f0e15bf3fb2c9dd8e44ded343907e93e84909
reddy <reddymeghraj@gmail.com>
2015-11-19 09:24:15 +0530

    [App for Lodge]

---
+++ lodge/lodge/doctype/add_room/add_room.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2013, Wayzon and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class AddRoom(Document):
+	def validate(self):
+		room=self.room_no
+		q=frappe.db.sql("""select room_no from `tabAdd Room` where room_no=%s""",(room))
+		if q:
+			frappe.throw("Entered Room No already exists!")
